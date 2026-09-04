8fe780d071a395396cdc7a12bffcc2817222c2f1
MEYRA <hongmeichen1219@gmail.com>
2026-09-04 18:56:43 +0800

    feat: official Canva Hozana Lovable Manus icons

---
+++ src/app/brandImages.ts
@@ -0,0 +1,11 @@
+import { CANVA } from './icons/canva';
+import { HOZANA } from './icons/hozana';
+import { LOVABLE } from './icons/lovable';
+import { MANUS } from './icons/manus';
+
+export const APP_IMAGES: Record<string, string> = {
+  Canva: CANVA,
+  Hozana: HOZANA,
+  Lovable: LOVABLE,
+  Manus: MANUS,
+};
