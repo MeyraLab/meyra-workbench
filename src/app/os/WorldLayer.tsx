ba05bc6915fcc813e55cd5a796583acfcfe15fde
MEYRA <hongmeichen1219@gmail.com>
2026-09-12 11:16:32 +0000

    Things-like 4K day sky and white clouds

--- src/app/os/WorldLayer.tsx
+++ src/app/os/WorldLayer.tsx
@@ -48,9 +48,9 @@ export function WorldLayer() {
       <img className="os-sky-img is-night" src="/world/sky-night-2.jpg" alt="" />
       <img className="os-sky-img is-day" src="/world/sky-day-2.jpg" alt="" />
       <div className="os-stars" />
-      <img className="os-cloud c1" src="/world/cloud-a.png" alt="" />
-      <img className="os-cloud c2" src="/world/cloud-b.png" alt="" />
-      <img className="os-cloud c3" src="/world/cloud-a.png" alt="" />
+      <img className="os-cloud c1" src="/world/cloud-white-a.webp" alt="" />
+      <img className="os-cloud c2" src="/world/cloud-white-b.webp" alt="" />
+      <img className="os-cloud c3" src="/world/cloud-white-c.webp" alt="" />
     </div>
   );
 }
